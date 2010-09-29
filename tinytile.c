6960a0f02fa29ac26983bb6fdde067ccc19512ca
Yamada Yohei <yamadayohei@gmail.com>
2010-09-30 01:05:56 +0900

    新たに map するウィンドウは画面の左半分を占有する

--- tinytile.c
+++ tinytile.c
@@ -1,13 +1,15 @@
 #include <X11/Xlib.h>
 #include <stdio.h>
 Display *Dpy;
-Screen   Scr;
 Window   Root;
+int      Scr;
 
 void
 mainloop ()
 {
    XEvent e;
+   Window wn;
+   unsigned int w, h;
 
    XSelectInput (Dpy, Root, SubstructureRedirectMask);
    for (;;) {
@@ -20,8 +22,12 @@ mainloop ()
          puts ("Configure");
          break;
       case MapRequest:
-         printf ("Map %08x\n", e.xmaprequest.window);
-         XMapRaised (Dpy, e.xmaprequest.window);
+         wn = e.xmaprequest.window;
+         printf ("Map %08x\n", wn);
+         w = DisplayWidth (Dpy, Scr) / 2;
+         h = DisplayHeight (Dpy, Scr);
+         XMoveResizeWindow (Dpy, wn, 0, 0, w, h);
+         XMapRaised (Dpy, wn);
          break;
       default:
          puts ("e");
@@ -33,7 +39,9 @@ int
 main ()
 {
    Dpy = XOpenDisplay (NULL);
-   Root = XDefaultRootWindow (Dpy);
+   if (!Dpy) return 1;
+   Scr = DefaultScreen (Dpy);
+   Root = DefaultRootWindow (Dpy);
    mainloop ();
    XCloseDisplay (Dpy);
    return 0;
